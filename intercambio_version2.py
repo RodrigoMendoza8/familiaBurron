49e90983d30ce7479f381bceb336fb9c7a599869
RodrigoMendoza8 <rodrigomendozardgz824@gmail.com>
2025-07-10 15:52:56 -0600

    Creacion del grafo, nodos y conexiones

--- intercambio_version2.py
+++ intercambio_version2.py
@@ -6,12 +6,37 @@ mendonza = ['Letos','Alberto','Rodrigo','Karla','Lucy']
 vidal = ['Conrado','Gina','Jesus']
 boston = ['Lupis','Mau']
 chilangos = ['Carmen','Gabriel','Gaby','Javier']
-familia = []
-#for i in range(4):
+familia = mendonza + vidal + boston + chilangos
+familias = [mendonza, vidal, boston, chilangos]
 
 G = nx.Graph()
-G.add_nodes_from([mendonza, vidal, boston, chilangos])
+G.add_nodes_from(familia)
 
-fig, ax = plt.subplots(figsize=(1,1))
-nx.draw(G, with_labels=True, ax=ax)
-print(G)
+for i in range(len(familias)):
+    for j in range(i + 1, len(familias)):
+        for persona1 in familias[i]:
+            for persona2 in familias[j]:
+                G.add_edge(persona1, persona2)
+
+adjM = nx.adjacency_matrix(G)
+adjM = adjM.todense()
+print(adjM)
+
+# Asignar colores
+color_map = []
+for persona in familia:
+    if persona in mendonza:
+        color_map.append('red')
+    elif persona in vidal:
+        color_map.append('blue')
+    elif persona in boston:
+        color_map.append('green')
+    elif persona in chilangos:
+        color_map.append('orange')
+
+
+# Dibujar
+fig, ax = plt.subplots(figsize=(15, 15))
+pos = nx.spring_layout(G, seed=42)
+nx.draw(G, pos, with_labels=True, node_color=color_map, node_size=1000, ax=ax)
+#plt.show()
